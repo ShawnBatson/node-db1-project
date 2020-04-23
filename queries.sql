77eae820029d315c35d8fd681d133fdb2ccff306
Shadewalker1984 <Shadewalker1984@yahoo.com>
2020-04-22 20:04:16 -0500

    finished the stretch sql problems that I had time to do

--- queries.sql
+++ queries.sql
@@ -40,4 +40,11 @@ WHERE "ContactName" = "Bilbo Baggins"
 
 -- (Stretch) Find a query to discover how many different cities are stored in the Customers table. Repeats should not be double counted
 
+SELECT COUNT ( DISTINCT "City" ) AS "Number of unique cities"
+FROM Customer;
+
 -- (Stretch) Find all suppliers who have names longer than 20 characters. You can use `length(SupplierName)` to get the length of the name
+
+SELECT "CompanyName"
+FROM "Supplier"
+WHERE LENGTH(CompanyName) > 20
